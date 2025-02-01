ba0b31f62083c16b25fb187486911f01f476bf31
PrabhatPadvi <prabhatpadvi2004@gmail.com>
2025-02-01 22:18:14 +0530

    Add files via upload

---
+++ Task 3-Calculator/cal.js
@@ -0,0 +1,16 @@
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
